ab44955866b081bf42c9f2da1002cb7076fdc7d6
Paul Maier <pauljustus279@gmail.com>
2020-11-18 11:38:37 +0100

    Fixed object errors

--- app/Http/Controllers/SortStudentsController.php
+++ app/Http/Controllers/SortStudentsController.php
@@ -787,7 +787,7 @@ class SortStudentsController extends Controller
 
                     $student->save();
                 });
-            } else if ($leader_id != 0) {
+            } else if ($project->leader_id != 0) {
                 $project_object = Project::findOrFail($project->id);
                 $leader = $project_object->leader();
 
