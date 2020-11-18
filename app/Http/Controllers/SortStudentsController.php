2cde227c5868fd483de0264e8cbbe50fb2e8e527
Paul Maier <pauljustus279@gmail.com>
2020-11-18 11:43:09 +0100

    Fixed SortStudentsController

--- app/Http/Controllers/SortStudentsController.php
+++ app/Http/Controllers/SortStudentsController.php
@@ -791,6 +791,11 @@ class SortStudentsController extends Controller
                 $project_object = Project::findOrFail($project->id);
                 $leader = $project_object->leader();
 
+                if ($project_object->leader_type === 'App\Leader') {
+                    $leader->project_id = 0;
+                    $leader->save();
+                }
+
                 if ($leader->exists()) {
                     $leader->notify(new ProjectHasNotEnoughParticipants());
                 }
