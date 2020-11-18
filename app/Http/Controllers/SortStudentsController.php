97437392d24ec75ec7254c56b3f3e18ae358701e
Paul Maier <pauljustus279@gmail.com>
2020-11-18 11:56:37 +0100

    Fixed SortStudentsController

--- app/Http/Controllers/SortStudentsController.php
+++ app/Http/Controllers/SortStudentsController.php
@@ -787,7 +787,7 @@ class SortStudentsController extends Controller
 
                     $student->save();
                 });
-            } else if (Project::findOrFail($project->id)->leader_id != 0) {
+            } else if (Project::find($project->id)) {
                 $project_object = Project::findOrFail($project->id);
                 $leader = $project_object->leader();
 
