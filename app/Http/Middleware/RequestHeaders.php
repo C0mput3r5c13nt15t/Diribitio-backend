2a714660bab53c790c37695a0d6313e68db89eaa
C0mput3r5c13nt15t <pauljustus279@gmail.com>
2020-12-29 19:46:40 +0100

    Fixed semicolons

--- app/Http/Middleware/RequestHeaders.php
+++ app/Http/Middleware/RequestHeaders.php
@@ -17,8 +17,8 @@ class RequestHeaders
     public function handle($request, Closure $next)
     {
       return $next($request)
-        ->header('Access-Control-Allow-Origin', '*') # Ändern!
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        ->header('Access-Control-Allow-Origin', '*'); # Ändern!
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
 }
