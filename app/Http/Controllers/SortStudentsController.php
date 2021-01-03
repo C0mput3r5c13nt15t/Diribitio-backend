0e11059a959ba6bc65950c58cb26ac8596d80b71
C0mput3r5c13nt15t <pauljustus279@gmail.com>
2021-01-03 12:06:11 +0100

    Fix email notifications after sorting

--- app/Http/Controllers/SortStudentsController.php
+++ app/Http/Controllers/SortStudentsController.php
@@ -801,12 +801,13 @@ class SortStudentsController extends Controller
                         $leader->project_id = 0;
                         $leader->save();
                     } else {
-                        collect($project->assistant_student_leaders)->each(function ($item)  use ($project_object) {
+                        collect($project->assistant_student_leaders)->each(function ($item) use ($project_object) {
                             $leader = (object) $item;
-                            if ($leader->id == $project_object->leader_id) {
-                                $leader->notify(new StudentProjectHasNotEnoughParticipants());
+                            $student = Student::findOrFail($leader->id);
+                            if ($student->id == $project_object->leader_id) {
+                                $student->notify(new StudentProjectHasNotEnoughParticipants());
                             } else {
-                                $leader->notify(new AssistantProjectHasNotEnoughParticipants());
+                                $student->notify(new AssistantProjectHasNotEnoughParticipants());
                             }
                         });
                     }
