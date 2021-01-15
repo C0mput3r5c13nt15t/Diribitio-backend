1b7dc9738a8602738e0771123655b43fb1530be5
C0mput3r5c13nt15t <pauljustus279@gmail.com>
2021-01-15 21:33:06 +0100

    performance test 100 2 3 5

--- database/factories/StudentsFactory.php
+++ database/factories/StudentsFactory.php
@@ -19,9 +19,9 @@ $factory->define(App\Student::class, function (Faker $faker) {
         'first_friend' => 0,
         'second_friend' => 0,
         'third_friend' => 0,
-        'first_wish' => $faker->numberBetween(1, 10),       # Danger : Hardcoded !!!!
-        'second_wish' => $faker->numberBetween(1, 10),
-        'third_wish' => $faker->numberBetween(1, 10),
+        'first_wish' => $faker->numberBetween(1, 5),       # Danger : Hardcoded !!!!
+        'second_wish' => $faker->numberBetween(1, 5),
+        'third_wish' => $faker->numberBetween(1, 5),
         'project_id' => 0,
         'role' => 1,
     ];
