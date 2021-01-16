fd20522b805ffb557790d17b04e99de039605eca
C0mput3r5c13nt15t <pauljustus279@gmail.com>
2021-01-16 11:08:28 +0100

    performance test 10000 250 250 500

--- database/factories/ProjectsFactory.php
+++ database/factories/ProjectsFactory.php
@@ -14,14 +14,14 @@ $factory->define(App\Project::class, function (Faker $faker) {
         'leader_name' => $faker->title . ' ' . $faker->lastName,
         'leader_id' => 0,
         'leader_type' => 'App\Leader',
-        'cost' => random_int(0, 5),
+        'cost' => 0,
         'first_day_begin' => $faker->time,
         'first_day_end' => $faker->time,
         'second_day_begin' => $faker->time,
         'second_day_end' => $faker->time,
-        'min_grade' => random_int(5, 7),
-        'max_grade' => random_int(7, 11),
+        'min_grade' => 5,
+        'max_grade' => 11,
         'min_participants' => 1,
-        'max_participants' => random_int(25, 45)
+        'max_participants' => 45
     ];
 });
